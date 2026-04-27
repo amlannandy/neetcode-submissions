06c4a459286aa422e50d3e44556cf4b07be8b35c
Amlan Kumar Nandy <45410599+amlannandy@users.noreply.github.com>
2026-04-27 16:00:38 +0530

    Add: top-k-elements-in-list - submission-2

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-2.js
@@ -0,0 +1,37 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+        const freqArray = new Array(nums.length+1).fill(null);
+        const freqMap = {};
+        for (let i=0; i<nums.length; i++) {
+            if (!freqMap[nums[i]]) {
+                freqMap[nums[i]] = 0;
+            }
+            freqMap[nums[i]] += 1;
+        }
+        Object.keys(freqMap).forEach(key => {
+            if (!freqArray[freqMap[key]]) {
+                freqArray[freqMap[key]] = [];
+            }
+            freqArray[freqMap[key]].push(key);
+        });
+        
+        const results = [];
+        let count = 0;
+        for (let i=freqArray.length-1; i>=0; i--) {
+            if (freqArray[i]) {
+                results.push(...freqArray[i]);
+                count += freqArray[i].length;
+            }
+            if (count >= k) {
+                break;
+            }
+        }
+
+        return results;
+    }
+}
